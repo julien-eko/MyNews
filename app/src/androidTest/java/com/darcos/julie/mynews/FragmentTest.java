0b68ac2ffa91bbe8324e9b0bbfba99d3b140eb5f
julie-eko <julien.darcos@gmail.com>
2019-04-19 13:02:33 +0200

    test 3

--- app/src/androidTest/java/com/darcos/julie/mynews/FragmentTest.java
+++ app/src/androidTest/java/com/darcos/julie/mynews/FragmentTest.java
@@ -18,54 +18,6 @@ import io.reactivex.observers.TestObserver;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
-
-@RunWith(AndroidJUnit4.class)
 public class FragmentTest {
-
-    //test api TopStories
-    @Test
-    public void topStoriesTest() throws Exception {
-        Observable<TopStories> obsTopStrories = TimesStreams.streamTopStories("food");
-
-        TestObserver<TopStories> testObserver = new TestObserver<>();
-
-        obsTopStrories.subscribeWith(testObserver)
-                .assertNoErrors()
-                .assertNoTimeout()
-                .awaitTerminalEvent();
-
-        TopStories test = testObserver.values().get(0);
-        assertThat("status is ok",test.getStatus().equals("OK"));
-    }
-
-    //test MostPopular api
-    public void MostPopularTest() throws Exception {
-        Observable<MostPopular> obsMostPopular = TimesStreams.streamMostPopular();
-
-        TestObserver<MostPopular> testObserver = new TestObserver<>();
-
-        obsMostPopular.subscribeWith(testObserver)
-                .assertNoErrors()
-                .assertNoTimeout()
-                .awaitTerminalEvent();
-
-        MostPopular test = testObserver.values().get(0);
-        assertThat("status is ok",test.getStatus().equals("OK"));
-    }
-
-    //test api Search
-    public void ArticleSearchTest() throws Exception {
-        Observable<Search> obsSeartch = TimesStreams.streamSearch("sports","news_desk:(\"Sports\" )","20190101","20190303");
-
-        TestObserver<Search> testObserver = new TestObserver<>();
-
-        obsSeartch.subscribeWith(testObserver)
-                .assertNoErrors()
-                .assertNoTimeout()
-                .awaitTerminalEvent();
-
-        Search test = testObserver.values().get(0);
-        assertThat("status is ok",test.getStatus().equals("OK"));
-    }
 
 }
